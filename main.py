cb72cb99700ce14cb355332b6d9e401e641650f7
samturton2 <STurton@spartaglobal.com>
2020-11-06 15:04:23 +0000

    scrabble class finished

---
+++ main.py
@@ -0,0 +1,10 @@
+# import scrabble class
+from scrabble_class import Scrabble
+
+# create main file, that takes in a string and returns the scrabble score
+if __name__ == "__main__":
+    # create an object with the scrabble class
+    scrabble_calculator = Scrabble()
+
+    # run the calculator on function
+    scrabble_calculator.calculator_on()
--- scrabble_class.py
+++ scrabble_class.py
@@ -1,14 +1,34 @@
 # create parent class that stores scrabble scores
-class Scrabble
+class Scrabble:
     def __init__(self):
         self.scrab_dict = {
-            1: [A, E, I, O, U, L, N, R, S, T],
-            2: [D, G],
-            3: [B, C, M, P],
-            4: [F, H, V, W, Y],
-            5: [K],
-            8: [J, X],
-            10: [Q, Z]
+            'A': 1, 'E': 1, 'I': 1, 'L': 1, 'N': 1, 'O': 1,'R': 1, 'S': 1, 'T': 1, 'U': 1,
+            'D': 2, 'G': 2,
+            'B': 3, 'C': 3, 'M': 3, 'P': 3,
+            'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4,
+            'K': 5,
+            'J': 8, 'X': 8,
+            'Q': 10, 'Z': 10
         }
 
-    def score(self):
+    # function that adds up the total score of the inputted string
+    def score(self, word):
+        total = 0
+
+        # adds up the letters corresponding score
+        for letter in word:
+            total += self.scrab_dict[letter.upper()]
+
+        return total
+
+    # create a function that takes in the users word and gives out the score as long as they want
+    def calculator_on(self):
+        # loop round outputting user score
+        while True:
+            word = input("Input word ===> ")
+            print(f"Scrabble score : {self.score(word)}")
+
+            # ask user if they want to do another word, if not, break
+            ask = input("Do you want to calculate another word? ")
+            if ask[0].lower() == "n":
+                break
